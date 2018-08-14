3db75d2efc1c3fb626bd3dc5c6edcc5f43e66b29
Asia Antczak <aasiaantczak@gmail.com>
2018-08-14 16:31:11 +0100

    Return first stanza

--- src/com/tengreenbottles/Song.java
+++ src/com/tengreenbottles/Song.java
@@ -2,5 +2,15 @@ package com.tengreenbottles;
 
 public class Song {
 
-
+    public String play() {
+        String lyrics = "Ten green bottles\n" +
+                "Hanging on the wall\n" +
+                "Ten green bottles\n" +
+                "Hanging on the wall\n" +
+                "And if one green bottle\n" +
+                "Should accidentally fall\n" +
+                "There'll be nine green bottles\n" +
+                "Hanging on the wall";
+        return lyrics;
+    }
 }
--- test/com/tengreenbottles/SongTest.java
+++ test/com/tengreenbottles/SongTest.java
@@ -1,7 +1,23 @@
 package com.tengreenbottles;
 
-import static org.junit.jupiter.api.Assertions.*;
 
-class SongTest {
+import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SongTest {
+
+    @Test
+    public void returnTheSongForTenBottles() {
+        Song song = new Song();
+        String lyrics = "Ten green bottles\n" +
+                "Hanging on the wall\n" +
+                "Ten green bottles\n" +
+                "Hanging on the wall\n" +
+                "And if one green bottle\n" +
+                "Should accidentally fall\n" +
+                "There'll be nine green bottles\n" +
+                "Hanging on the wall";
+        assertEquals(lyrics, song.play());
+    }
 }
